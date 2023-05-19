4b16697e2ab18742ec229a65e3e83c6203afff17
kero-sene <johnm.vergis@gmail.com>
2023-05-19 14:24:05 -0700

    Idk anymore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Weather from "./pages/weather"
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter, Route} from 'react-router-dom';
 const App = () => {
   return (
     <>
     <HashRouter basename='/'>
-    <Routes>
       <Route exact path="/"></Route>
       <Route path='/weather' component={<Weather />}> </Route>
-    </Routes>
     </HashRouter>
     </>
   );
